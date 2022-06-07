b0b243ab5c2194a9406fe00fd83ce773f9bbd9a9
GoktayIncekara <goktayincekara@gmail.com>
2022-06-07 13:13:19 +0300

    Final version of the project

---
+++ alice.py
@@ -0,0 +1,160 @@
+#260201037 - 250201012
+#When compiling the code in the ubuntu terminal, python3 should be added in the beginning of each command
+#For example: >>python3 alice.py dhke -a 4 -B 10 -p 23
+
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import pad
+import base64
+
+import sys
+from random import randint
+
+BLOCK_SIZE = 32
+
+#color codes for the extra part
+START_GREEN = '\033[92m'
+START_RED = '\033[93m'
+START_CYAN = '\033[96m'
+BOLD = '\033[1m'
+END_COLOR = '\033[0m'    
+ 
+           
+def checkPrime(num):
+    if(num==0 or num==1):
+        return False
+    for i in range(2, num):
+        if (num%i == 0):
+            return False
+        
+    return True  
+
+def primitiveRoot(num,prime):
+    control = set()
+    for i in range(1,prime):
+        number = (num**i) % prime
+        control.add(number)
+    if (len(control)==(prime-1)):
+        return True
+    else:
+        return False
+    
+def randomNumber():
+    return randint(1,10)
+
+def trailingSpaces(plainText):
+    trailingSpaces = len(plainText) % 8 #takes the remainder of the division of the plain text's length with 8
+    trailingSpaces= 8- int(trailingSpaces)  #and finds the needed number to complete the length to a multiply of 8 
+    return plainText + (b' ' * trailingSpaces)
+
+def main():
+    p=0
+    g=0
+    a=0
+    B=0
+    A=0
+    s=0
+    k=0
+    
+    if (sys.argv[1]=="dhke"):
+        if(len(sys.argv)==6):
+            if(sys.argv[2] == "-g" and sys.argv[4]=="-p"):
+                g = sys.argv[3]
+                p = sys.argv[5]
+            elif (sys.argv[2] == "-p" and sys.argv[4]=="-g"):
+                p = sys.argv[3]
+                g = sys.argv[5]
+            else:
+                raise Exception("Please provide p and g to initialize the key.")    
+                
+
+            p= int(p)
+            g= int(g)
+            
+            if (checkPrime(p)== False):
+                print("This is not a prime number. Exiting the system!")
+                sys.exit()
+            else:
+                print("p = ", p ,START_GREEN + 'OK' + END_COLOR, START_RED +"(This is a prime number.)"+ END_COLOR)
+                
+            if (primitiveRoot(g,p)==False):
+                print("This is not a primitive root. Exiting the system!")
+                sys.exit()
+            else:
+                print("g = ", g ,START_GREEN + 'OK' + END_COLOR, START_RED +"(This is a primitive root modulo ", p,".)"+END_COLOR)
+            print("Alice and Bob publicly agree on the values of p and g.")
+            print("However, it is advised to use any pair of p and g only once.")
+                
+            a = randomNumber()  #generates private key of Alice as a random number 
+            A= (g**a)%p  #computes the public key of Alice
+            print("a = ", a, BOLD + " (This must be kept secret.)" + END_COLOR)
+            print("A = ", A, BOLD + " (This can be sent to Bob.)" + END_COLOR)
+
+        elif (len(sys.argv)==8):
+            if(sys.argv[2] == "-a" and sys.argv[4]=="-B" and sys.argv[6]=="-p"):
+                a = sys.argv[3]
+                B = sys.argv[5]
+                p = sys.argv[7]
+            elif(sys.argv[2] == "-a" and sys.argv[4]=="-p" and sys.argv[6]=="-B"):
+                a = sys.argv[3]
+                p = sys.argv[5]
+                B = sys.argv[7]
+            elif(sys.argv[2] == "-p" and sys.argv[4]=="-a" and sys.argv[6]=="-B"):
+                p = sys.argv[3]
+                a = sys.argv[5]
+                B = sys.argv[7]
+            elif(sys.argv[2] == "-p" and sys.argv[4]=="-B" and sys.argv[6]=="-a"):
+                p = sys.argv[3]
+                B = sys.argv[5]
+                a = sys.argv[7]
+            elif(sys.argv[2] == "-B" and sys.argv[4]=="-p" and sys.argv[6]=="-a"):
+                B = sys.argv[3]
+                p = sys.argv[5]
+                a = sys.argv[7]
+            elif(sys.argv[2] == "-B" and sys.argv[4]=="-a" and sys.argv[6]=="-p"):
+                B = sys.argv[3]
+                a = sys.argv[5]
+                p = sys.argv[7]
+            else:
+                raise Exception("Please provide a, p and B to calculate the key.")
+                
+            B = int(B) 
+            a = int(a)
+            p = int(p)    
+            
+            s = (B**a)%p  #generates the secret number between Bob and Alice
+            print(START_RED +"s = " + END_COLOR, s )
+            print("This must be kept secret. However, Bob should be able to calculate this as well.")
+        else:
+            raise Exception("Please provide p and g to initialize the key exchange or provide a,p and B to calculate the key.")
+    
+    elif (sys.argv[1]=="des"):
+
+        if(sys.argv[2] == "-p" and sys.argv[4]=="-k"):
+            p = sys.argv[3]
+            k = sys.argv[5]
+        elif (sys.argv[2] == "-k" and sys.argv[4]=="-p"):
+            k = sys.argv[3]
+            p = sys.argv[5]
+        else:
+            raise Exception("Please provide the plaintext and a key.")    
+            
+        zero_amount= 8- len(k)  #the zero amount which will be needed to complete the key's length to 8
+        byte_key=k
+        
+        for i in range(0,zero_amount):  #puts the zeros to the beginning of the key
+            byte_key= '0'+byte_key 
+            
+        key=bytes(byte_key, 'ascii')     #converts string to byte       
+                
+        encodedPlain = bytes(p, 'ascii')  #converts string to byte
+        trailing_plain=trailingSpaces(encodedPlain)  #adds space at the end of the plain text if needed
+        
+        cipher = DES.new(key, DES.MODE_ECB)  
+        msg = cipher.encrypt(pad(trailing_plain, BLOCK_SIZE)) #encrypt des function with using ECB mode
+        print(START_CYAN+'Raw ciphertext (Normally this is sent to Bob over a network):\n'+END_COLOR, msg)
+        cip_dec = base64.b64encode(msg) #encode raw ciphertest to readable ciphertext
+        readable_cip= cip_dec.decode()
+        print(START_CYAN+'Readable ciphertext (For the sake of simplicity, we will send this to Bob. This can also be used if we use pen and paper to deliver the message.):\n'+ END_COLOR, readable_cip)
+
+        
+main()        
